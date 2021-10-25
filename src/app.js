53e90a1292d3f0033140e6eb00dab931e1cbd5f6
William Hamilton <draxy80@gmail.com>
2021-10-26 08:42:43 +1000

    Updated app

--- src/app.js
+++ src/app.js
@@ -26,19 +26,3 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 module.exports = app;
-
-/*
-
-  / --> GET --> res server running
-  /lists/:userid --> GET --> res lists for userid
-  /sharedlists/userid --> GET --> res lists shared to userid
-  /signin --> POST --> res user
-  /register --> POST --> res user
-  /createlist --> POST --> res list
-  /updatepassword/:userid --> POST --> res user 
-  /createlistitem/:listid/ --> POST --> res list item
-  /updatelist/:listid --> PUT --> res list
-  /updatelistitem/:listid/:listitemid --> PUT --> res list item
-  /updateuser/:userid --> PUT --> res user
-
- */
