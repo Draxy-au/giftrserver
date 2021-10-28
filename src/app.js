30c05dd350f63de85dbafd7beb29a1fe87bfe2a6
William Hamilton <draxy80@gmail.com>
2021-10-28 17:45:36 +1000

    added cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const middlewares = require('./middlewares');
 const api = require('./api');
@@ -13,6 +14,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(compression());
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
